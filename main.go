d91e60399a2d5b94282e29eea378f74c44d8e12c
nqirod <nqirod@gmail.com>
2018-03-12 18:33:25 +0100

    Update main.go

--- main.go
+++ main.go
@@ -15,20 +15,20 @@ import (
 )
 
 var (
-	// Version will be set at build time.
-	Version       = "0.0.0.dev"
+	// La version sera définie au build time.
+	Version       = "0.0.1.dev"
 	listenAddress = flag.String("web.listen-address", ":9161", "Address to listen on for web interface and telemetry.")
 	metricPath    = flag.String("web.telemetry-path", "/metrics", "Path under which to expose metrics.")
 	landingPage   = []byte("<html><head><title>Oracle DB Exporter " + Version + "</title></head><body><h1>Oracle DB Exporter " + Version + "</h1><p><a href='" + *metricPath + "'>Metrics</a></p></body></html>")
 )
 
-// Metric name parts.
+// Nom racine des métriques.
 const (
 	namespace = "oracledb"
 	exporter  = "exporter"
 )
 
-// Exporter collects Oracle DB metrics. It implements prometheus.Collector.
+// L'exporter collecte les metriques. Il implémente prometheus.Collector.
 type Exporter struct {
 	dsn             string
 	duration, error prometheus.Gauge
@@ -37,7 +37,7 @@ type Exporter struct {
 	up              prometheus.Gauge
 }
 
-// NewExporter returns a new Oracle DB exporter for the provided DSN.
+// NewExporter retourne un nouveau exporter Oracle DB pour les DSN fournis.
 func NewExporter(dsn string) *Exporter {
 	return &Exporter{
 		dsn: dsn,
@@ -140,9 +140,24 @@ func (e *Exporter) scrape(ch chan<- prometheus.Metric) {
 	isUpRows.Close()
 	e.up.Set(1)
 
+	if err = ScrapeSessions(db, ch); err != nil {
+		log.Errorln("Error scraping for sessions:", err)
+		e.scrapeErrors.WithLabelValues("sessions").Inc()
+	}
+
 	if err = ScrapeActivity(db, ch); err != nil {
 		log.Errorln("Error scraping for activity:", err)
 		e.scrapeErrors.WithLabelValues("activity").Inc()
+	}
+
+	if err = ScrapeBaseStatus(db, ch); err != nil {
+		log.Errorln("Error scraping for status:", err)
+		e.scrapeErrors.WithLabelValues("status").Inc()
+	}
+
+	if err = ScrapeFRA(db, ch); err != nil {
+		log.Errorln("Error scraping for fra:", err)
+		e.scrapeErrors.WithLabelValues("fra").Inc()
 	}
 
 	if err = ScrapeTablespace(db, ch); err != nil {
@@ -150,30 +165,16 @@ func (e *Exporter) scrape(ch chan<- prometheus.Metric) {
 		e.scrapeErrors.WithLabelValues("tablespace").Inc()
 	}
 
-	if err = ScrapeWaitTime(db, ch); err != nil {
-		log.Errorln("Error scraping for wait_time:", err)
-		e.scrapeErrors.WithLabelValues("wait_time").Inc()
-	}
-
-	if err = ScrapeSessions(db, ch); err != nil {
-		log.Errorln("Error scraping for sessions:", err)
-		e.scrapeErrors.WithLabelValues("sessions").Inc()
-	}
-	
-	if err = ScrapeFRA(db, ch); err != nil {
-		log.Errorln("Error scraping for fra:", err)
-		e.scrapeErrors.WithLabelValues("fra").Inc()
-	}	
-	
-	if err = ScrapeRMAN(db, ch); err != nil {
-		log.Errorln("Error scraping for rman:", err)
-		e.scrapeErrors.WithLabelValues("rman").Inc()
-	}
-	
 	if err = ScrapeUsers(db, ch); err != nil {
 		log.Errorln("Error scraping for users:", err)
 		e.scrapeErrors.WithLabelValues("users").Inc()
 	}
+
+	if err = ScrapeRMAN(db, ch); err != nil {
+		log.Errorln("Error scraping for rman:", err)
+		e.scrapeErrors.WithLabelValues("rman").Inc()
+	}	
+
 
 }
 
@@ -235,34 +236,6 @@ func ScrapeSessions(db *sql.DB, ch chan<- prometheus.Metric) error {
 	return nil
 }
 
-// ScrapeWaitTime collects wait time metrics from the v$waitclassmetric view.
-func ScrapeWaitTime(db *sql.DB, ch chan<- prometheus.Metric) error {
-	var (
-		rows *sql.Rows
-		err  error
-	)
-	rows, err = db.Query("SELECT n.wait_class, round(m.time_waited/m.INTSIZE_CSEC,3) AAS from v$waitclassmetric  m, v$system_wait_class n where m.wait_class_id=n.wait_class_id and n.wait_class != 'Idle'")
-	if err != nil {
-		return err
-	}
-	defer rows.Close()
-	for rows.Next() {
-		var name string
-		var value float64
-		if err := rows.Scan(&name, &value); err != nil {
-			return err
-		}
-		name = cleanName(name)
-		ch <- prometheus.MustNewConstMetric(
-			prometheus.NewDesc(prometheus.BuildFQName(namespace, "wait_time", name),
-				"Generic counter metric from v$waitclassmetric view in Oracle.", []string{}, nil),
-			prometheus.CounterValue,
-			value,
-		)
-	}
-	return nil
-}
-
 // ScrapeActivity collects activity metrics from the v$sysstat view.
 func ScrapeActivity(db *sql.DB, ch chan<- prometheus.Metric) error {
 	var (
@@ -292,121 +265,107 @@ func ScrapeActivity(db *sql.DB, ch chan<- prometheus.Metric) error {
 	return nil
 }
 
-// ScrapeTablespace collects tablespace size.
+// ScrapeBaseStatus collects session metrics from the v$session view.
+func ScrapeBaseStatus(db *sql.DB, ch chan<- prometheus.Metric) error {
+	var (
+		rows *sql.Rows
+		err  error
+	)
+	rows, err = db.Query("SELECT instance_name as name, status from v$instance")
+	if err != nil {
+		return err
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		var status string
+		var name string
+		var value float64
+		if err := rows.Scan(&name, &status); err != nil {
+			return err
+		}
+		if status == "OPEN" {
+			value = 2
+		} else
+		if status == "MOUNTED" {
+			value = 1
+		} else {
+		value = 0
+		}
+			ch <- prometheus.MustNewConstMetric(
+			prometheus.NewDesc(prometheus.BuildFQName(namespace, "status", name),
+				"Statut de la base", []string{}, nil),
+				prometheus.CounterValue,
+				value,
+		)
+	}
+	return nil
+}
+
+// ScrapeTablespace collects tablespace used space and if it's autoextensible.
 func ScrapeTablespace(db *sql.DB, ch chan<- prometheus.Metric) error {
 	var (
 		rows *sql.Rows
 		err  error
 	)
 	rows, err = db.Query(`
-SELECT
-  Z.name,
-  dt.status,
-  dt.contents,
-  dt.extent_management,
-  Z.bytes,
-  Z.max_bytes,
-  Z.free_bytes
-FROM
-(
-  SELECT
-    X.name                   as name,
-    SUM(nvl(X.free_bytes,0)) as free_bytes,
-    SUM(X.bytes)             as bytes,
-    SUM(X.max_bytes)         as max_bytes
-  FROM
-    (
-      SELECT
-        ddf.tablespace_name as name,
-        ddf.status as status,
-        ddf.bytes as bytes,
-        sum(dfs.bytes) as free_bytes,
-        CASE
-          WHEN ddf.maxbytes = 0 THEN ddf.bytes
-          ELSE ddf.maxbytes
-        END as max_bytes
-      FROM
-        sys.dba_data_files ddf,
-        sys.dba_tablespaces dt,
-        sys.dba_free_space dfs
-      WHERE ddf.tablespace_name = dt.tablespace_name
-      AND ddf.file_id = dfs.file_id(+)
-      GROUP BY
-        ddf.tablespace_name,
-        ddf.file_name,
-        ddf.status,
-        ddf.bytes,
-        ddf.maxbytes
-    ) X
-  GROUP BY X.name
-  UNION ALL
-  SELECT
-    Y.name                   as name,
-    MAX(nvl(Y.free_bytes,0)) as free_bytes,
-    SUM(Y.bytes)             as bytes,
-    SUM(Y.max_bytes)         as max_bytes
-  FROM
-    (
-      SELECT
-        dtf.tablespace_name as name,
-        dtf.status as status,
-        dtf.bytes as bytes,
-        (
-          SELECT
-            ((f.total_blocks - s.tot_used_blocks)*vp.value)
-          FROM
-            (SELECT tablespace_name, sum(used_blocks) tot_used_blocks FROM gv$sort_segment WHERE  tablespace_name!='DUMMY' GROUP BY tablespace_name) s,
-            (SELECT tablespace_name, sum(blocks) total_blocks FROM dba_temp_files where tablespace_name !='DUMMY' GROUP BY tablespace_name) f,
-            (SELECT value FROM v$parameter WHERE name = 'db_block_size') vp
-          WHERE f.tablespace_name=s.tablespace_name AND f.tablespace_name = dtf.tablespace_name
-        ) as free_bytes,
-        CASE
-          WHEN dtf.maxbytes = 0 THEN dtf.bytes
-          ELSE dtf.maxbytes
-        END as max_bytes
-      FROM
-        sys.dba_temp_files dtf
-    ) Y
-  GROUP BY Y.name
-) Z, sys.dba_tablespaces dt
-WHERE
-  Z.name = dt.tablespace_name
+select distinct dba_tablespace_usage_metrics.tablespace_name, dba_data_files.autoextensible, trunc(dba_tablespace_usage_metrics.used_percent) from dba_tablespace_usage_metrics, dba_data_files
 `)
 	if err != nil {
 		return err
 	}
 	defer rows.Close()
-	tablespaceBytesDesc := prometheus.NewDesc(
-		prometheus.BuildFQName(namespace, "tablespace", "bytes"),
-		"Generic counter metric of tablespaces bytes in Oracle.",
-		[]string{"tablespace", "type"}, nil,
-	)
-	tablespaceMaxBytesDesc := prometheus.NewDesc(
-		prometheus.BuildFQName(namespace, "tablespace", "max_bytes"),
-		"Generic counter metric of tablespaces max bytes in Oracle.",
-		[]string{"tablespace", "type"}, nil,
-	)
-	tablespaceFreeBytesDesc := prometheus.NewDesc(
+
+	tablespaceFreePercentDesc := prometheus.NewDesc(
 		prometheus.BuildFQName(namespace, "tablespace", "free"),
 		"Generic counter metric of tablespaces free bytes in Oracle.",
-		[]string{"tablespace", "type"}, nil,
+		[]string{"tablespace_name", "autoextensible"}, nil,
 	)
 
 	for rows.Next() {
 		var tablespace_name string
-		var status string
-		var contents string
-		var extent_management string
-		var bytes float64
-		var max_bytes float64
-		var bytes_free float64
+		var autoextensible string
+		var percent_free float64
 
-		if err := rows.Scan(&tablespace_name, &status, &contents, &extent_management, &bytes, &max_bytes, &bytes_free); err != nil {
+		if err := rows.Scan(&tablespace_name, &autoextensible, &percent_free); err != nil {
 			return err
 		}
-		ch <- prometheus.MustNewConstMetric(tablespaceBytesDesc, prometheus.GaugeValue, float64(bytes), tablespace_name, contents)
-		ch <- prometheus.MustNewConstMetric(tablespaceMaxBytesDesc, prometheus.GaugeValue, float64(max_bytes), tablespace_name, contents)
-		ch <- prometheus.MustNewConstMetric(tablespaceFreeBytesDesc, prometheus.GaugeValue, float64(bytes_free), tablespace_name, contents)
+		ch <- prometheus.MustNewConstMetric(tablespaceFreePercentDesc, prometheus.GaugeValue, float64(percent_free), tablespace_name, autoextensible)
+	}
+	return nil
+}
+
+
+func ScrapeUsers(db *sql.DB, ch chan<- prometheus.Metric) error {
+	var (
+		rows *sql.Rows
+		err  error
+	)
+	// Retrieve status and type for all sessions.
+	rows, err = db.Query("SELECT TRUNC(expiry_date) - TRUNC(sysdate) as expiration, username, REPLACE(account_status, ' ', '') FROM dba_users")
+	if err != nil {
+		return err
+	}
+
+	defer rows.Close()
+
+	for rows.Next() {
+		var (
+			expiration float64
+			username string
+			account_status string
+		)
+		if err := rows.Scan(&expiration, &username, &account_status); err != nil {
+			return err
+		}
+		ch <- prometheus.MustNewConstMetric(
+			prometheus.NewDesc(prometheus.BuildFQName(namespace, "users", "expiration"),
+				"Utilisateurs", []string{"username", "status"}, nil),
+			prometheus.CounterValue,
+			expiration,
+			username,
+			account_status,
+		)
 	}
 	return nil
 }
@@ -470,12 +429,12 @@ func ScrapeRMAN(db *sql.DB, ch chan<- prometheus.Metric) error {
 		}
 
 		if bkp_status == "COMPLETED" {
-			Value = 2
+			Value = 0	// Green value
 		} else
 		if bkp_status == "RUNNING" {
-			Value = 1
+			Value = 1 // Yellow value
 		} else {
-		Value = 0
+		Value = 2 // Red value
 		}
 
 		ch <- prometheus.MustNewConstMetric(
@@ -494,39 +453,6 @@ func ScrapeRMAN(db *sql.DB, ch chan<- prometheus.Metric) error {
 	return nil
 }
 
-func ScrapeUsers(db *sql.DB, ch chan<- prometheus.Metric) error {
-	var (
-		rows *sql.Rows
-		err  error
-	)
-	// Retrieve status and type for all sessions.
-	rows, err = db.Query("SELECT TRUNC(expiry_date) - TRUNC(sysdate) as expiration, username, REPLACE(account_status, ' ', '') FROM dba_users")
-	if err != nil {
-		return err
-	}
-
-	defer rows.Close()
-
-	for rows.Next() {
-		var (
-			expiration float64
-			username string
-			account_status string
-		)
-		if err := rows.Scan(&expiration, &username, &account_status); err != nil {
-			return err
-		}
-		ch <- prometheus.MustNewConstMetric(
-			prometheus.NewDesc(prometheus.BuildFQName(namespace, "users", "expiration"),
-				"Utilisateurs", []string{"username", "status"}, nil),
-			prometheus.CounterValue,
-			expiration,
-			username,
-			account_status,
-		)
-	}
-	return nil
-}
 
 // Oracle gives us some ugly names back. This function cleans things up for Prometheus.
 func cleanName(s string) string {
